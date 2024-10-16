e1c243001d53b48fff692545f426950f1be25145
surajverma2587 <surajverma2587@gmail.com>
2024-10-16 18:25:44 +0100

    wip

---
+++ src/data/movies.js
@@ -0,0 +1,182 @@
+module.exports = [
+  {
+    "id": "98030a14-2b5c-42a5-af94-4bf6a182859c",
+    "title": "Inception",
+    "year": 2010,
+    "genre": "Sci-Fi",
+    "description": "A thief who enters the dreams of others to steal secrets from their subconscious.",
+    "director": "Christopher Nolan",
+    "rating": 8.8
+  },
+  {
+    "id": "9bde0d6c-b288-4922-91e3-5ac38f6bbbf7",
+    "title": "The Shawshank Redemption",
+    "year": 1994,
+    "genre": "Drama",
+    "description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "director": "Frank Darabont",
+    "rating": 9.3
+  },
+  {
+    "id": "edbf4782-a4ff-4d73-996a-58afcd554404",
+    "title": "Pulp Fiction",
+    "year": 1994,
+    "genre": "Crime",
+    "description": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    "director": "Quentin Tarantino",
+    "rating": 8.9
+  },
+  {
+    "id": "94db3cbd-9d0e-4ae4-876e-1654e446f4b3",
+    "title": "The Godfather",
+    "year": 1972,
+    "genre": "Crime",
+    "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    "director": "Francis Ford Coppola",
+    "rating": 9.2
+  },
+  {
+    "id": "e988bc07-54e1-4607-8b17-f15c70344dec",
+    "title": "The Dark Knight",
+    "year": 2008,
+    "genre": "Action",
+    "description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    "director": "Christopher Nolan",
+    "rating": 9
+  },
+  {
+    "id": "c398818b-d023-4de6-9ed6-a81ec1fc1240",
+    "title": "Schindler's List",
+    "year": 1993,
+    "genre": "Biography",
+    "description": "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+    "director": "Steven Spielberg",
+    "rating": 8.9
+  },
+  {
+    "id": "7910d4d9-2b1c-4092-936e-6209fe1885fb",
+    "title": "Forrest Gump",
+    "year": 1994,
+    "genre": "Drama",
+    "description": "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+    "director": "Robert Zemeckis",
+    "rating": 8.8
+  },
+  {
+    "id": "31ba0d80-dad7-48e0-abb8-653d1683dc41",
+    "title": "The Matrix",
+    "year": 1999,
+    "genre": "Sci-Fi",
+    "description": "A computer programmer discovers that reality as he knows it is a simulation created by machines to subjugate humanity.",
+    "director": "The Wachowskis",
+    "rating": 8.7
+  },
+  {
+    "id": "0aecd6b3-2da5-4a0d-8a53-64fce7460045",
+    "title": "Goodfellas",
+    "year": 1990,
+    "genre": "Crime",
+    "description": "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
+    "director": "Martin Scorsese",
+    "rating": 8.7
+  },
+  {
+    "id": "c60360ad-1a3b-40d7-b0c9-62e9e87074de",
+    "title": "The Silence of the Lambs",
+    "year": 1991,
+    "genre": "Thriller",
+    "description": "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.",
+    "director": "Jonathan Demme",
+    "rating": 8.6
+  },
+  {
+    "id": "1ccbcd32-25de-4363-b788-a32019a10895",
+    "title": "Fight Club",
+    "year": 1999,
+    "genre": "Drama",
+    "description": "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into something much, much more.",
+    "director": "David Fincher",
+    "rating": 8.8
+  },
+  {
+    "id": "57c80d90-cffc-4f93-a393-74c815e91e0b",
+    "title": "The Lord of the Rings: The Fellowship of the Ring",
+    "year": 2001,
+    "genre": "Fantasy",
+    "description": "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.",
+    "director": "Peter Jackson",
+    "rating": 8.8
+  },
+  {
+    "id": "640a42a3-cefd-474c-8b97-27efff0ac55f",
+    "title": "Interstellar",
+    "year": 2014,
+    "genre": "Sci-Fi",
+    "description": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    "director": "Christopher Nolan",
+    "rating": 8.6
+  },
+  {
+    "id": "c3830293-b9b2-40fb-8e8e-3b444443a0e4",
+    "title": "The Green Mile",
+    "year": 1999,
+    "genre": "Drama",
+    "description": "The lives of guards on Death Row are affected by one of their charges: a black man accused of child murder and rape, yet who has a mysterious gift.",
+    "director": "Frank Darabont",
+    "rating": 8.6
+  },
+  {
+    "id": "5190adb4-ad02-4d9f-b404-af4f1f27d07a",
+    "title": "Saving Private Ryan",
+    "year": 1998,
+    "genre": "War",
+    "description": "Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
+    "director": "Steven Spielberg",
+    "rating": 8.6
+  },
+  {
+    "id": "daef1e23-e182-4ba3-812b-4a1b155f7c54",
+    "title": "The Departed",
+    "year": 2006,
+    "genre": "Crime",
+    "description": "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.",
+    "director": "Martin Scorsese",
+    "rating": 8.5
+  },
+  {
+    "id": "2023e8f1-47f2-48f2-9c62-2dac09ac8b5a",
+    "title": "Gladiator",
+    "year": 2000,
+    "genre": "Action",
+    "description": "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+    "director": "Ridley Scott",
+    "rating": 8.5
+  },
+  {
+    "id": "653451a1-aefa-46a1-a3ee-a9c1a7749458",
+    "title": "The Usual Suspects",
+    "year": 1995,
+    "genre": "Crime",
+    "description": "A sole survivor tells of the twisty events leading up to a horrific gun battle on a boat, which began when five criminals met at a seemingly random police lineup.",
+    "director": "Bryan Singer",
+    "rating": 8.5
+  },
+  {
+    "id": "1aa71cd4-6bb1-4095-8aba-9f1eb474c64a",
+    "title": "The Lion King",
+    "year": 1994,
+    "genre": "Animation",
+    "description": "A young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery.",
+    "director": "Roger Allers, Rob Minkoff",
+    "rating": 8.5
+  },
+  {
+    "id": "ecd18d0f-6aa9-45ce-af5e-0705461813bb",
+    "title": "Whiplash",
+    "year": 2014,
+    "genre": "Drama",
+    "description": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.",
+    "director": "Damien Chazelle",
+    "rating": 8.5
+  }
+]
--- src/index.js
+++ src/index.js
@@ -1,28 +1,83 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+
+let movies = require("./data/movies");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 const PORT = 4000;
 
-// register route
 app.get("/movies", (req, res) => {
-  res.send("GET movies");
+  return res.json({
+    data: movies,
+  });
 });
 
 app.get("/movies/:id", (req, res) => {
-  res.send("GET movie");
+  const id = req.params.id;
+
+  const movie = movies.find((movie) => {
+    return movie.id === id;
+  });
+
+  if (!movie) {
+    return res.status(404).json({
+      error: "Movie not found",
+    });
+  }
+
+  return res.json({
+    data: movie,
+  });
 });
 
 app.post("/movies", (req, res) => {
-  res.send("POST movie");
+  const newMovie = req.body;
+
+  newMovie.id = uuidv4();
+
+  movies.push(newMovie);
+
+  return res.json({
+    data: newMovie,
+  });
 });
 
 app.put("/movies/:id", (req, res) => {
-  res.send("PUT movie");
+  const payload = req.body;
+  const id = req.params.id;
+
+  movies = movies.map((movie) => {
+    if (movie.id === id) {
+      movie = {
+        ...movie,
+        ...payload,
+      };
+
+      return movie;
+    } else {
+      return movie;
+    }
+  });
+
+  return res.json({
+    data: "Movie updated",
+  });
 });
 
 app.delete("/movies/:id", (req, res) => {
-  res.send("DELETE movie");
+  const id = req.params.id;
+
+  movies = movies.filter((movie) => {
+    return movie.id !== id;
+  });
+
+  return res.json({
+    data: "Movie deleted",
+  });
 });
 
 app.listen(PORT, () => {
