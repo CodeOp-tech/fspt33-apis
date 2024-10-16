e42cf8b5a3add8e890c2abdfe8f9dba371186e2e
surajverma2587 <surajverma2587@gmail.com>
2024-10-16 10:36:00 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("hello from nodeJS");
+const express = require("express");
+
+const app = express();
+
+const PORT = 4000;
+
+// register route
+app.get("/movies", (req, res) => {
+  res.send("GET movies");
+});
+
+app.get("/movies/:id", (req, res) => {
+  res.send("GET movie");
+});
+
+app.post("/movies", (req, res) => {
+  res.send("POST movie");
+});
+
+app.put("/movies/:id", (req, res) => {
+  res.send("PUT movie");
+});
+
+app.delete("/movies/:id", (req, res) => {
+  res.send("DELETE movie");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
